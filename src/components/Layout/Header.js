7c8aa2b4c30c1c6363a2cb810d0922061e49eb8c
picapipicca <haewon09106@gmail.com>
2022-01-12 16:35:24 +0900

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, {Fragment} from 'react';
 import classes from "./Header.module.css";
 
 import mealsImage from "../../assets/Meals.jpeg";
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <Fragment>
                 <header className={classes.header}>
                     <h1>Haewon's Meals</h1>
-                    <button>Cart</button>
+                    <HeaderCartButton/>
                 </header>
                 <div className={classes["main-image"]}>
                     <img src={mealsImage} alt="cafe highwaste in Jongro, Seoul" />
